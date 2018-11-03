a912d133a41502f75691ca7096cb77c786557116
AltheFlyer <30613228+AltheFlyer@users.noreply.github.com>
2018-11-03 10:50:19 -0400

    Update sketch_test.ino

---
+++ sketch_test.ino
@@ -0,0 +1,40 @@
+#include <Stepper.h>
+int serialSensor = 0;
+int serialTemp = 1;
+int ledPin = 6;
+int t = 0;
+const int stepsPerRevolution = 4096;
+int stepCount = 0;
+Stepper myStepper(stepsPerRevolution,8,9,10,11);
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
+  myStepper.setSpeed(8);
+  Serial.begin(9600);
+}
+
+void loop() {
+//  // put your main code here, to run repeatedly:
+//  int rawLight = analogRead(serialSensor);
+//  int rawTemp = analogRead(serialTemp);
+//  Serial.print("Light: ");
+//  Serial.println(rawLight);
+  
+  myStepper.step(2000);
+
+  
+//  float R = 1023.0/rawTemp-1.0;
+//  float B = 4275;
+//  float temperature = 1.0/(log(R)/B+1/298.15)-273.15;
+//  Serial.print("Temperature: ");
+//  Serial.println(temperature);
+//
+//  digitalWrite(ledPin, HIGH);
+//  if (rawLight > 100) {
+//    digitalWrite(ledPin, HIGH);
+//    Serial.println("BRIGHT");
+//  }
+  Serial.println("rotates");
+  delay(1000);
+}
