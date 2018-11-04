ff5eb738fa654941344bfea1128b94476bdf05d8
AltheFlyer <30613228+AltheFlyer@users.noreply.github.com>
2018-11-03 20:02:29 -0400

    Update serial_reader.py

--- serial_reader.py
+++ serial_reader.py
@@ -7,8 +7,21 @@ soil_set = []
 temp_set = []
 
 
+#Convert input to byte stream
+#Soil threshold
+reader.write(input().encode())
+#Frequency
+reader.write(input().encode())
 
 while True:
-    code = reader.readline()
-    print(code)
-    code = code[2:]
+    code = str(reader.readline())
+    if 'Soil' in code:
+        value = int(code[5:])
+        print(value)
+    elif 'Light' in code:
+        value = int(code[7:])
+    elif 'Temperature' in code:
+        value = int(code[12:])
+    else:
+        value = ""
+    print(value)
